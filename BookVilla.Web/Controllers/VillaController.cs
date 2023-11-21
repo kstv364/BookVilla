e0623b37ab9f19e8554816e9084741738424dd78
Kaustav Chanda <Kaustav.Chanda@hyland.com>
2023-11-22 02:32:28 +0530

    CRUD operations completed and added static files

--- BookVilla.Web/Controllers/VillaController.cs
+++ BookVilla.Web/Controllers/VillaController.cs
@@ -27,9 +27,59 @@ namespace BookVilla.Web.Controllers
         [HttpPost]
         public IActionResult Create(Villa obj)
         {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
+
             if (ModelState.IsValid)
             {
                 _context.Villas.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _context.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _context.Villas.Update(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _context.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _context.Villas.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+                _context.Villas.Remove(objFromDb);
                 _context.SaveChanges();
                 return RedirectToAction("Index");
             }
