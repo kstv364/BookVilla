52db240f6ac468e6d0fe9d10612d7a902f9a7ba2
Kaustav Chanda <Kaustav.Chanda@hyland.com>
2023-11-22 01:36:56 +0530

    Display Data Annotation

--- BookVilla.Domain/Entities/Villa.cs
+++ BookVilla.Domain/Entities/Villa.cs
@@ -1,13 +1,17 @@
-﻿namespace BookVilla.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookVilla.Domain.Entities
 {
     public class Villa
     {
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price per night")]
         public double Price { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? Created_Date { get; set; }
         public DateTime? Updated_Date { get; set; }
