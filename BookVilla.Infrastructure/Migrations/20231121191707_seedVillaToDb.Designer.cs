033e4ae5dce6f296c19c8d483007bc7b5dd53e1b
Kaustav Chanda <Kaustav.Chanda@hyland.com>
2023-11-22 00:49:31 +0530

    Seed Villa data to Db

--- BookVilla.Infrastructure/Migrations/20231121191707_seedVillaToDb.Designer.cs
+++ BookVilla.Infrastructure/Migrations/20231121191707_seedVillaToDb.Designer.cs
@@ -4,6 +4,7 @@ using BookVilla.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookVilla.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231121191707_seedVillaToDb")]
+    partial class seedVillaToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
