42a8c000d212c6bd840438759bc25d108dc419dd
Kaustav Chanda <Kaustav.Chanda@hyland.com>
2023-11-22 11:14:15 +0530

    Using ViewModels to pass data from Controller to View

---
+++ BookVilla.Web/ViewModels/VillaNumberViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BookVilla.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookVilla.Web.ViewModels
+{
+    public class VillaNumberViewModel
+    {
+        public VillaNumber? VillaNumber { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? VillaList { get; set; }
+    }
+}
