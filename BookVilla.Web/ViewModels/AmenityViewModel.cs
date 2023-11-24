bf69cf328eabe7c567d83a26b26e3ac39dbe5c99
Kaustav Chanda <Kaustav.Chanda@hyland.com>
2023-11-24 20:42:36 +0530

    Amenity Controller and Views

---
+++ BookVilla.Web/ViewModels/AmenityViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BookVilla.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookVilla.Web.ViewModels
+{
+    public class AmenityViewModel
+    {
+        public Amenity? Amenity { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? VillaList { get; set; }
+    }
+}
